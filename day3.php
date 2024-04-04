47882bf4902a4413cf5bdb21292f0fe4c56db65f
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2024-04-04 22:00:33 +0530

    day4 done

--- day3.php
+++ day3.php
@@ -168,7 +168,7 @@
     // var_dump($fruits);
     // $fruits[] = "Orange";
     // var_dump($fruits);
-
+    
     // $cars = array("brand" => "Ford", "model" => "Mustang");
     // $cars["color"] = "Red";
     // var_dump($cars);
@@ -176,28 +176,50 @@
     // $cars = array("brand" => "Ford", "model" => "Mustang");
     // $cars += ["color" => "red", "year" => 1964];
     // var_dump($cars);
-
+    
     // $cars = array("Volvo", "BMW", "Toyota");
     // array_splice($cars, 1, 1); //specify the index (where to start) and how many items you want to delete.
     // var_dump($cars);
-
+    
     //function to remove items from an associative array.
     // $cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964); //The array_diff() function takes values as parameters, and not keys.
     // $newarray = array_diff($cars, ["Mustang", 1964]);
-
+    
     // $cars = array("Volvo", "BMW", "Toyota");
     // array_pop($cars);//removes the last item of an array.
     // array_shift($cars);//removes the first item of an array.
-
+    
     // -------------------------------------------------------------------------------------
-
+    
     // sort() - sort arrays in ascending order
     // rsort() - sort arrays in descending order
     // asort() - sort associative arrays in ascending order, according to the value
     // ksort() - sort associative arrays in ascending order, according to the key
     // arsort() - sort associative arrays in descending order, according to the value
     // krsort() - sort associative arrays in descending order, according to the key
+    
+    // -------------------------------------------------------------------------------------
+    
+    // $cars = array(
+    //     array("Volvo", 22, 18),
+    //     array("BMW", 15, 13),
+    //     array("Saab", 5, 2),
+    //     array("Land Rover", 17, 15)
+    // );
 
+    // echo $cars[0][0] . ": In stock: " . $cars[0][1] . ", sold: " . $cars[0][2] . ".<br>";
+    // echo $cars[1][0] . ": In stock: " . $cars[1][1] . ", sold: " . $cars[1][2] . ".<br>";
+    // echo $cars[2][0] . ": In stock: " . $cars[2][1] . ", sold: " . $cars[2][2] . ".<br>";
+    // echo $cars[3][0] . ": In stock: " . $cars[3][1] . ", sold: " . $cars[3][2] . ".<br>";
+
+    // for ($row = 0; $row < 4; $row++) {
+    //     echo "<p><b>Row number $row</b></p>";
+    //     echo "<ul>";
+    //     for ($col = 0; $col < 3; $col++) {
+    //         echo "<li>" . $cars[$row][$col] . "</li>";
+    //     }
+    //     echo "</ul>";
+    // }
 
     ?>
 </body>

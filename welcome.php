298695c3d663674ca1c72f6fe102ce6ffa230494
AmanSangani <127185092+AmanSangani@users.noreply.github.com>
2024-04-05 18:02:37 +0530

    Forms

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Welcome
+        <?php echo $_POST["name"]; ?>
+    </title>
+</head>
+
+<body>
+    Welcome
+    <?php echo $_POST["name"]; ?><br>
+    Your email address is:
+    <?php echo $_POST["email"]; ?>
+</body>
+
+</html>
